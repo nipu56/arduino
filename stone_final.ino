819e810595ce1839a787c4c8d0c1ed18216c0a0e
nipu56 <49304305+nipu56@users.noreply.github.com>
2022-06-16 15:31:52 +0900

    좆두이노 최종

--- stone_final.ino
+++ stone_final.ino
@@ -1,41 +1,36 @@
 #include <Adafruit_NeoPixel.h>
 #include<SoftwareSerial.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+ #include <avr/power.h>
 #endif
 #define PIN            13
 #define NUMPIXELS      3
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int r;
-int g;
-int b;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // 네오픽셀 핀설정, 픽셀의 개수, 핀의 위치
+String r;
+String g;
+String b;
 
 SoftwareSerial BTSerial = SoftwareSerial(9,10);
 void setup() {
   pixels.begin();
-  Serial.begin(115200);
   BTSerial.begin(115200);
   
 }
 
 void loop() {
    if (BTSerial.available() > 0) { // 수신받은 데이터가 2개 초과일 때
-    String strData = BTSerial.readStringUntil('\n');
-    Serial.println(strData.toInt());
-    r = (strData.toInt() / 1000000);
-    g = (strData.toInt() / 1000 - r * 1000);
-    b = (strData.toInt() - r * 1000000 - g * 1000) ; 
- 
-
-
-    Serial.println(r);
-    Serial.println(g);
-    Serial.println(b);
     
-  
-     for(int i=0;i<NUMPIXELS;i++){
-      pixels.setPixelColor(i, pixels.Color(r,g,b));
+    String strData = BTSerial.readStringUntil('\n');
+    int first = strData.indexOf(",");
+    int second = strData.indexOf(",",first+1);
+    
+    r = (strData.substring(0,first));
+    g = (strData.substring(first+1,second));
+    b = (strData.substring(second+1,strData.length())) ; 
+    
+    for(int i=0;i<NUMPIXELS;i++){
+     pixels.setPixelColor(i, pixels.Color(r.toInt(),g.toInt(),b.toInt()));
       pixels.show(); //
       delay(500);
     }
